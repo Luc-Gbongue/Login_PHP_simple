9be0c800e337ee78c2f925506e03b3c0976b0fc0
Luc <zgbongue@gmail.com>
2026-05-20 23:16:19 +0000

    premier commit

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page_accueil</title>
+</head>
+<body>
+<h1>Bienvenue sur notre site!</h1>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,55 @@
+<?php 
+//Demarrer la session PHP
+session_start();
+
+// verifier si formulaire est envoyé
+if($_SERVER["REQUEST_METHOD"] === "POST"){
+
+// Recuperer les données entrées
+$nomUtilisateur = $_POST["username"];
+$motdePasse = $_POST["password"];
+
+// verifier si les informations saisies sont correctes
+if($nomUtilisateur === "example@gmail.com" && $motdePasse === "1234"){
+
+// Ouvrir la session de l'utilisateur en stockant son username
+$_SESSION["user"] = $nomUtilisateur;
+
+// Afficher message de connexion
+echo("Vous êtes connecté!");
+
+//  Et stopper le script
+header("Location: accueil.php");
+
+}
+
+else{
+    echo("Nom d'utilisateur ou Mot de passe incorrect");
+}
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion_PHP</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Connexion</h1>
+        <form action="#" method="POST">
+            <input type="email" name="username" placeholder="Entrez votre nom d'utilisateur">
+
+            <input type="password" name="password" placeholder="Entrez votre mot de passe">
+
+            <button type="submit">Se connecter</button>
+
+        </form>
+    </div>
+</body>
+</html>
